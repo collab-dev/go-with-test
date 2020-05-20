00b69fb80fa8438deb9500a65eb1f81dcfce71ec
Gabor Szabo <gabor@szabgab.com>
2020-05-20 16:05:44 +0300

    app with test

---
+++ calc_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	total := add(2, 2)
+	if total != 4 {
+		t.Error("Sum was incorrect")
+	}
+}
+
+// func TestAddMore(t *testing.T) {
+// 	total := add(3, 3)
+// 	if total != 6 {
+// 		t.Error("Sum was incorrect")
+// 	}
+// }
